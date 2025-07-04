9814eaed5ed9a390015dbd770b99f49153f2c6b3
chino2606 <je9323192@gmail.com>
2025-07-04 13:15:28 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Suma de Polinomios</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+
+    <center><h1>Centro de Estudios Científicos y Tecnológicos del Estado de México</h1></center>
+    <center><h1>CECYTEM</h1></center>
+  
+   <center>La suma de polinomios es una operación en álgebra que consiste en combinar dos o más polinomios <br>
+    para obtener un nuevo polinomio. <br>
+    Este nuevo polinomio se forma sumando los términos semejantes (términos con la misma parte literal y exponente) <br>
+    de los polinomios originales. </center>
+  <img src="poli.png" alt="logo">
+
+    <table>
+        <tr>
+            <th>Caracteristicas </th>
+        </tr>
+        <tr>
+            <td>
+                Combinación de términos semejantes:
+Solo se pueden sumar términos que tienen la misma parte literal (misma variable elevada a la misma potencia). <br>
+Suma de coeficientes:
+Se suman los coeficientes de los términos semejantes, y la parte literal se mantiene. <br>
+Propiedad conmutativa:
+El orden en que se suman los polinomios no afecta el resultado (A + B = B + A). <br>
+Propiedad asociativa:
+La forma de agrupar los polinomios al sumarlos no afecta el resultado ((A + B) + C = A + (B + C)). 
+                <br><br>
+            </td>
+        </tr>
+    </table>
+
+    <table>
+        <tr>
+            <th>Para que sirve</th>
+        </tr>
+        <tr>
+            <td>
+               Simplificación de expresiones:
+Permite reducir varias expresiones polinómicas a una sola expresión más compacta, lo que facilita su manejo y análisis. <br>
+Resolución de ecuaciones:
+La suma de polinomios es un paso fundamental en la solución de ecuaciones polinómicas, donde se busca encontrar los valores de las variables que satisfacen la igualdad. <br>
+Modelado matemático:
+En diversas disciplinas como la física, la ingeniería y la economía, los polinomios se utilizan para modelar fenómenos y relaciones entre variables. La suma de polinomios permite combinar diferentes modelos para obtener uno más completo o representativo de un sistema. <br>
+Álgebra abstracta:
+En álgebra abstracta, los polinomios son elementos fundamentales para construir estructuras algebraicas más complejas, como anillos y campos. La suma de polinomios es una operación esencial dentro de estas estructuras. <br>
+Geometría:
+La suma de polinomios puede aplicarse en geometría para definir funciones que describen formas geométricas y sus relaciones. 
+                <br><br>
+            </td>
+        </tr>
+    </table>
+ 
+    <table>
+        <tr>
+            <th>Conclusion</th>
+        </tr>
+        <tr>
+            <td>
+           En síntesis, la suma de polinomios no es solo una operación mecánica, 
+           sino una herramienta poderosa que contribuye a la resolución de problemas y al entendimiento de múltiples fenómenos del mundo real. <br>
+            Su dominio es indispensable para cualquier persona que desee profundizar en el estudio de las matemáticas y sus aplicaciones prácticas en distintas disciplinas.
+            <br><br>
+                
+            </td>
+        </tr>
+    </table>
+
+    <h2>Ventajas</h2>
+    <ol>
+        <li>Simplificación de expresiones</li>
+        <li>Propiedades algebraicas</li>
+        <li>Aplicaciones en matemáticas</li>
+        <li>Modelado y resolución de problemas</li>
+        <li>Cálculo y análisis matemático</li>
+        
+    </ol>
+
+    <h2>Mas informacion</h2>
+
+    </form>
+    <form action="detalles.php" method="get" style="display:inline;">
+        <button type="submit">Detalles de la Suma de Polinomios</button>
+    </form>
+    <form action="ejemplos.php" method="get" style="display:inline;">
+        <button type="submit">Ejemplos</button>
+    </form>
+    <form action="propialge.php" method="get" style="display:inline;">
+        <button type="submit">Propiedades Algebraicas</button>
+    </form>
+<h2>Video de ayuda</h2>
+<a href="https://www.youtube.com/watch?v=IiOnygcPCh0" target="_blank">https://www.youtube.com/watch?v=IiOnygcPCh0</a>
+
+</body>
+</html>
---
+++ propialge.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Propiedades Algebraicas de los Polinomios</title>
+    <link rel="stylesheet" href="estilos.css">
+
+</head>
+<body>
+    <h1>Propiedades Algebraicas de los Polinomios</h1>
+
+    <h2></h2>
+    <ul>
+        <li> Conmutativa:
+El orden de los términos en la suma o multiplicación no altera el resultado. <br>
+Por ejemplo, para la suma: P(x) + Q(x) = Q(x) + P(x). Para la multiplicación: P(x) * Q(x) = Q(x) * P(x).</li>
+        <li> Asociativa:
+En la suma o multiplicación de varios polinomios, la forma en que se agrupan los términos no afecta el resultado. <br>
+Por ejemplo, para la suma: (P(x) + Q(x)) + R(x) = P(x) + (Q(x) + R(x)). Para la multiplicación: (P(x) * Q(x)) * R(x) = P(x) * (Q(x) * R(x)).</li>
+        <li>Distributiva:
+La multiplicación de un polinomio por otro se realiza aplicando la propiedad distributiva, es decir,
+ multiplicando cada término del primer polinomio por cada término del segundo.  </li>
+        <li> Suma:
+El grado del polinomio resultante de la suma de dos polinomios es, a lo sumo, igual al mayor de los grados de los polinomios sumados.</li>
+        <li> Multiplicación:
+El grado del polinomio resultante de la multiplicación de dos polinomios es igual a la suma de los grados de los polinomios multiplicados. </li>
+        <img src="poli3.jpg" alt="Mecatronica">
+    </ul>
+
+   
+<a href="index.php" class="button">Volver al inicio</a>
+
+</body>
+</html>
