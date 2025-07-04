6410eb198e72e98eb9525fb99079bc42d7d73cdf
chino2606 <je9323192@gmail.com>
2025-07-04 13:22:56 -0600

    Add files via upload

---
+++ examen/detalles.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Detalles de la Suma de Poinomios</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <h1>Pasos para Sumar Polinomios</h1>
+        <ul>
+        <li>Identificar términos semejantes: Busca los términos que tienen la misma variable con el mismo exponente. </li>
+        <li>Sumar los coeficientes: Suma los números que acompañan a los términos semejantes. </li>
+        <li>Escribir el resultado: El resultado de la suma de los coeficientes se escribe junto a la parte literal de los términos semejantes. </li>
+        <li>Repetir para todos los términos semejantes: Repite los pasos anteriores para todos los términos semejantes en los polinomios. </li>
+        <li>Escribir el polinomio resultante: Combina todos los resultados obtenidos para formar el nuevo polinomio.</li>
+        </ul>
+<img src="poli2.png" alt="Instrumentacion">
+    </ul>
+
+<a href="index.php" class="button">Volver al inicio</a>
+
+</body>
+</html>
---
+++ examen/ejemplos.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mantenimiento Automotriz</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <h1>Ejemplo</h1>
+    <ul>
+        <li>Ejemplo 1: Suma vertical</li>
+        <li>Considera los polinomios: P(x) = 3x² + 5x - 2, Q(x) = x² - 2x + 7. </li>
+        <li>Para sumarlos verticalmente, se alinean los términos semejantes y se suman sus coeficientes:</li>
+        <li>  3x² + 5x - 2 </li>
+        <li>+  x² - 2x + 7</li>
+        <li> ---------------- </li>
+        <li> 4x² + 3x + 5 </li>
+        <li> Ejemplo 2: Suma horizontal</li>
+        <li>Considera los polinomios: P(x) = 4x³ - 2x² + 6x - 1, Q(x) = -x³ + 7x² - 3x + 9. </li>
+        <li>Para sumarlos horizontalmente, se agrupan los términos semejantes y se suman:  </li>
+        <li>P(x) + Q(x) = (4x³ - x³) + (-2x² + 7x²) + (6x - 3x) + (-1 + 9) </li>
+        <li>P(x) + Q(x) = 3x³ + 5x² + 3x + 8 </li>
+        <li>Ejemplo 3: Suma de polinomios con diferentes grados </li>
+        <li>Considera los polinomios: P(x) = 5x⁴ + 2x² - 3x + 1, Q(x) = 3x³ - x + 5.</li>
+        <li>En este caso, aunque no haya un término x³ en P(x), se puede incluir un término 0x³ para facilitar la suma: </li>
+        <li> 5x⁴ + 0x³ + 2x² - 3x + 1 </li>
+        <li>+  0x⁴ + 3x³ + 0x² - x + 5 </li>
+        <li>-----------------------------</li>
+        <li>5x⁴ + 3x³ + 2x² - 4x + 6 </li>
+        <li>La suma de P(x) y Q(x) es 5x⁴ + 3x³ + 2x² - 4x + 6.</li>
+        <li> En resumen, la suma de polinomios implica:</li>
+        <li>Identificar términos semejantes: Aquellos con la misma variable y exponente. </li>
+        <li>Sumar los coeficientes de los términos semejantes.</li>
+        <li>Mantener la parte literal. </li>
+        <li>Si es necesario, ordenar los polinomios antes de sumar.</li>
+        <img src="poli2.png" alt="Mantenimiento Automotriz">
+    </ul>
+
+  <a href="index.php" class="button">Volver al inicio</a>
+
+</body>
+</html>
